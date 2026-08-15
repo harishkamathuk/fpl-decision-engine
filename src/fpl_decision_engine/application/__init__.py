3a1b7de1edb7c19324dff66be832ff5d87b19d98
Harish Kamath <github@harishkamath.com>
2026-08-15 22:36:03 +0100

    feat(application): persist GW1 squad decisions

--- src/fpl_decision_engine/application/__init__.py
+++ src/fpl_decision_engine/application/__init__.py
@@ -1,13 +1,25 @@
 """Application use cases and orchestration."""
 
 from .availability import apply_availability_exclusions, assess_availability
+from .decision_bundles import (
+    DecisionBundleArtifact,
+    build_decision_bundle,
+    serialize_decision_bundle,
+    write_decision_bundle,
+)
 from .planning import compare_planning_horizons, persist_planning_decision_run
+from .squad_runs import persist_squad_decision_run
 from .transfer_runs import persist_transfer_decision_run
 
 __all__ = [
+    "DecisionBundleArtifact",
     "apply_availability_exclusions",
     "assess_availability",
+    "build_decision_bundle",
     "compare_planning_horizons",
     "persist_planning_decision_run",
+    "persist_squad_decision_run",
     "persist_transfer_decision_run",
+    "serialize_decision_bundle",
+    "write_decision_bundle",
 ]
